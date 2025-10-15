0582f387ca9d96c0095009ca72759f52b61fffcd
Buchanz <tyler.buchanan.a@gmail.com>
2025-10-15 11:53:06 -0700

    Initial Commit

---
+++ Noctowl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Noctowl
+//
+//  Created by Tyler Buchanan on 2025-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Noctowl/NoctowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoctowlApp.swift
+//  Noctowl
+//
+//  Created by Tyler Buchanan on 2025-10-15.
+//
+
+import SwiftUI
+
+@main
+struct NoctowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
